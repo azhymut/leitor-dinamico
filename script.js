0599e8de9cd2e180171eaebba82642c467e27a7e
azhymut <110800461+azhymut@users.noreply.github.com>
2024-05-11 16:51:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-let currentPage = 97; // Starting with page 97
+let currentPage = 97;
 
 function changePage(step) {
     const prevPage = document.getElementById(`page-${currentPage}`);
     currentPage += step;
     const nextPage = document.getElementById(`page-${currentPage}`);
+    const currentPageDisplay = document.getElementById("current-page");
     if (prevPage) prevPage.style.display = 'none';
     if (nextPage) {
         nextPage.style.display = 'block';
+        currentPageDisplay.textContent = currentPage;
     } else {
-        currentPage -= step; // Revert change if no next/prev page
+        currentPage -= step;
     }
 }
 
 function toggleTheme() {
     const body = document.body;
+    const themeIcon = document.querySelector('.theme-icon');
     if (body.classList.contains("dark-mode")) {
         body.classList.replace("dark-mode", "light-mode");
+        themeIcon.innerHTML = '&#x1F4A1;';
     } else {
         body.classList.replace("light-mode", "dark-mode");
+        themeIcon.innerHTML = '&#x1F4A1;';
     }
 }
 
-document.getElementById(`page-${currentPage}`).style.display = 'block'; // Show initial page
+document.getElementById(`page-${currentPage}`).style.display = 'block';
