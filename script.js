777a91f9fdf267b2d47081f15f432af0bc9b20cc
azhymut <110800461+azhymut@users.noreply.github.com>
2024-05-12 02:30:28 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const nav = document.querySelector('.navigation');
+    let timer;
+
+    document.addEventListener('touchstart', () => {
+        nav.style.opacity = '1';
+        nav.style.visibility = 'visible';
+        clearTimeout(timer);
+    });
+
+    document.addEventListener('touchend', () => {
+        timer = setTimeout(() => {
+            nav.style.opacity = '0';
+            nav.style.visibility = 'hidden';
+        }, 2000); // Hide after 2 seconds of no interaction
+    });
+
     const themeButton = document.querySelector('.navigation button:first-child');
     const prevButton = document.querySelector('.navigation button:nth-child(2)');
     const nextButton = document.querySelector('.navigation button:nth-child(3)');
     const currentPageElement = document.getElementById("current-page");
 
-    function toggleTheme() {
+    themeButton.addEventListener('click', () => {
         document.body.classList.toggle("light-mode");
         document.body.classList.toggle("dark-mode");
-    }
+    });
 
     function changePage(direction) {
-        let currentPage = parseInt(currentPageElement.textContent, 10);
-        let newPage = currentPage + direction;
-        let newPageElement = document.getElementById(`page-${newPage}`);
-        if (newPageElement) {
-            document.querySelector('p[style*="display: block"]').style.display = 'none';
-            newPageElement.style.display = 'block';
-            currentPageElement.textContent = newPage;
+        let currentPageNumber = parseInt(currentPageElement.textContent);
+        let newPageNumber = currentPageNumber + direction;
+        let currentPage = document.getElementById(`page-${currentPageNumber}`);
+        let newPage = document.getElementById(`page-${newPageNumber}`);
+
+        if (newPage) {
+            currentPage.style.display = 'none';
+            newPage.style.display = 'block';
+            currentPageElement.textContent = newPageNumber;
         }
     }
 
-    themeButton.addEventListener('click', toggleTheme);
     prevButton.addEventListener('click', () => changePage(-1));
     nextButton.addEventListener('click', () => changePage(1));
 });
